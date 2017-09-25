183e23f80b90b284b6497970538a2924e68ced18
Kenneth Peiruza <kenneth@floss.cat>
2017-09-25 15:19:21 +0200

    Trying to bypass hostname in config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
-
+if [ -f "$HOST_ETC/hostname" ]
+    HOSTNAME="$(cat $HOST_ETC/hostname)"
+elif [ -f "$HOST_ETC/HOSTNAME" ]
+    HOSTNAME=$(cat $HOST_ETC/HOSTNAME)
+else
+    HOSTNAME=$(hostname)
+fi
+sed -i 's/hostname = .*/hostname = $HOSTNAME/' /etc/telegraf/telegraf.conf
 #hostname -F /etc/hostname
-export HOSTNAME=$(cat /etc/hostname)
+export HOSTNAME
 if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
 fi
